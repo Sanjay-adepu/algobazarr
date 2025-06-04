4ee365c5f082e21c36630201749cabe2ef4e25e4
Sanjay-adepu <adepusanjay812@gmail.com>
2025-06-04 14:05:43 +0530

    Update Home.jsx

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -10,13 +10,7 @@ const ProductCards = () => {
       <h2 className="customization-title">Customization</h2>
 
       <div className="products-grid">
-
-        <a
-          className="product-card"
-          href="https://forms.gle/izKtv55kMwYS7mei6"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="product-card" href="https://forms.gle/izKtv55kMwYS7mei6" target="_blank" rel="noopener noreferrer">
           <div className="product-image-wrapper">
             <img
               src="https://res.cloudinary.com/dppiuypop/image/upload/v1748232368/uploads/pgghdd1o984m3fph3jkh.jpg"
@@ -30,12 +24,7 @@ const ProductCards = () => {
           <div className="product-button">Click here</div>
         </a>
 
-        <a
-          className="product-card"
-          href="https://forms.gle/ttLjs1D612XzLa6LA"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="product-card" href="https://forms.gle/ttLjs1D612XzLa6LA" target="_blank" rel="noopener noreferrer">
           <div className="product-image-wrapper">
             <img
               src="https://res.cloudinary.com/dppiuypop/image/upload/v1748254107/uploads/onpjlu15vkpsuzgjmz2n.jpg"
@@ -48,7 +37,6 @@ const ProductCards = () => {
           <div className="product-price">₹ 1</div>
           <div className="product-button">Click here</div>
         </a>
-
       </div>
 
       <Footer />
