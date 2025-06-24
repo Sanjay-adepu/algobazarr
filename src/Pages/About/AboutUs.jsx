26a7016e3e6382eb0d9917c5b2e6372ab8d68152
Sanjay-adepu <adepusanjay812@gmail.com>
2025-06-24 17:02:31 +0530

    Update AboutUs.jsx

--- src/Pages/About/AboutUs.jsx
+++ src/Pages/About/AboutUs.jsx
@@ -17,20 +17,19 @@ const AboutUs = () => {
           </h2>
 
           <p>
-            AlgoBazarr was born out of a passion to bridge the gap between trading ideas and automated execution. Since July 2021, we’ve been dedicated to transforming trader concepts into fully automated strategies using the Tradetron platform.
+            AlgoDukaan was born out of a passion to bridge the gap between trading ideas and automated execution. Since July 2021, we’ve been dedicated to transforming trader concepts into fully automated strategies using the Tradetron platform.
           </p>
           <p>
             We understand that building algo trading strategies can be complex and time-consuming. That’s why our focus is on delivering fast, reliable, and customized solutions that help traders automate their strategies effortlessly. By combining our technical skills with deep market understanding, we enable traders to take control of their trades with confidence.
           </p>
           <p>
-            At AlgoBazarr, our commitment is to support traders at every step—from concept to live deployment—ensuring a seamless and efficient algo trading experience.
+            At AlgoDukaan, our commitment is to support traders at every step—from concept to live deployment—ensuring a seamless and efficient algo trading experience.
           </p>
 
           <div className="review-images">
             {/* Replace with real images later */}
             <img src="./file_0000000097f861fdafa53801ef03516d.png" alt="Customer Review 1" />
             <img src="./file_00000000ad3c61f8b64fdac3990191d7.png" alt="Customer Review 2" />
-
           </div>
         </div>
       </section>
